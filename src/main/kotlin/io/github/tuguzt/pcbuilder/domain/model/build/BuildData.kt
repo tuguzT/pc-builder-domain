6d6a91083722c3c543b090b9353a47eef4437ec2
tuguzT <timurka.tugushev@gmail.com>
2022-06-07 18:43:51 +0300

    Add build data

---
+++ src/main/kotlin/io/github/tuguzt/pcbuilder/domain/model/build/BuildData.kt
@@ -0,0 +1,24 @@
+package io.github.tuguzt.pcbuilder.domain.model.build
+
+import io.github.tuguzt.pcbuilder.domain.interactor.randomNanoId
+import io.github.tuguzt.pcbuilder.domain.model.NanoId
+import io.github.tuguzt.pcbuilder.domain.model.component.data.*
+import kotlinx.serialization.EncodeDefault
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+public data class BuildData(
+    @EncodeDefault override val id: NanoId = randomNanoId(),
+    override val name: String,
+    override val case: CaseData?,
+    override val cooler: CoolerData?,
+    override val centralProcessingUnit: CpuData?,
+    override val graphicsProcessingUnit: List<GpuData>,
+    override val memory: List<MemoryData>,
+    override val monitor: List<MonitorData>,
+    override val motherboard: MotherboardData?,
+    override val powerSupplyUnit: PsuData?,
+    override val storage: List<StorageData>,
+) : Build
